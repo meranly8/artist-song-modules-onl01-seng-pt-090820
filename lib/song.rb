78875b50ce7d5c34a410bfbca84993bfba042e98
Michelle Ranly <meranly8@gmail.com>
2020-10-08 06:49:26 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,7 +4,7 @@ class Song
   extend Memorable::ClassMethods
   include Memorable::InstanceMethods
   include Paramable
-  include Findable
+  extend Findable::ClassMethods
   
   attr_accessor :name
   attr_reader :artist
