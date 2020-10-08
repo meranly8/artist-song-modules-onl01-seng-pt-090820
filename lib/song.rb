c3461ad611dd93b20ce3866f1bced3f2c6157568
Michelle Ranly <meranly8@gmail.com>
2020-10-08 06:46:35 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,15 +4,16 @@ class Song
   extend Memorable::ClassMethods
   include Memorable::InstanceMethods
   include Paramable
+  include Findable
   
   attr_accessor :name
   attr_reader :artist
 
   @@songs = []
 
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
+  # def self.find_by_name(name)
+  #   @@songs.detect{|a| a.name == name}
+  # end
 
   def self.all
     @@songs
